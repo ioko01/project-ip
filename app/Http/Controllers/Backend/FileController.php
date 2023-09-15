3658f65364cc8e1bb7ea121832609cee789b3115
Your Name <ioko.peezaza@gmail.com>
2023-09-15 16:25:05 +0700

    commit

---
+++ app/Http/Controllers/Backend/FileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    //
+
+    public function resizeImage(Request $request)
+    {
+        
+    }
+}
